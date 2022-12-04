3a65dff842a7e35e6609a30b32fb473cf414614d
peterbrok <peternovikov75@gmail.com>
2022-12-04 12:08:19 +0200

    fix

--- src/com/bookstore/datasource/reader/FileDataReader.java
+++ src/com/bookstore/datasource/reader/FileDataReader.java
@@ -1,2 +1,12 @@
-package com.bookstore.datasource.reader;public class FileDataReader {
+package com.bookstore.datasource.reader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+public abstract class FileDataReader extends BaseDataReader{
+
+    protected Reader getFileReader(String filePath) throws FileNotFoundException {
+        return new FileReader(filePath);
+    }
 }
